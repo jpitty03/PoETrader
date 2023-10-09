85736efed719ce62dcbf84238ce9af6d369c2e64
jpitty <jpitty03@gmail.com>
2023-10-08 20:35:23 -0700

    init commit

---
+++ PoETrader/src/test/java/com/jpitty/PoETrader/PoETraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpitty.PoETrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoETraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
